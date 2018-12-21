4de8abac098b93cb412781001c05c5a89db1f2ee
waheng chang <wahengchang@gmail.com>
2018-12-21 18:27:12 +0800

    created TimePicker.vue

---
+++ packages/dateTimePicker/src/components/TimePicker.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="timeWrapper">
+    time picker
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DateTimePicker",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+  .loadingFixBackground{
+    position: fixed;
+    width: 115vw;
+    height: 1115vh;
+    background: rgba(50,50,50,0.6);
+    z-index: 99;
+  }
+</style>
