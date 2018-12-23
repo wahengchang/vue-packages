902638a4cb66b6230f538cca959e02cc341fffd7
AnnaHuang <anna.huang@oriente.com>
2018-12-23 20:08:34 +0800

    add date picker UI

--- packages/dateTimePicker/src/components/DatePicker.vue
+++ packages/dateTimePicker/src/components/DatePicker.vue
@@ -1,9 +1,43 @@
 <template>
-  <div></div>
+  <div>
+    <div class="datePicker_wrap">
+      <div class="calendar-header">
+        <span class="left">Left</span>
+        <h3>Month 2018</h3>
+        <span class="right">Right</span>
+      </div>
+      <ul class="calendar">
+        <li class="day" v-for="(day, index) in weekDay" :key="index">
+          {{ day }}
+        </li>
+        <li class="day" v-for="(day, index) in 35" :key="index">{{ day }}</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "DatePicker",
+
+  data() {
+    return {
+      weekDay: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    };
+  }
+};
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.calendar-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+ul.calendar {
+  li.day {
+    display: inline-block;
+    width: calc(100% / 7);
+  }
+}
+</style>
