249060c1fec599b45e863c825580c4305aa9723b
waheng chang <wahengchang@gmail.com>
2018-12-27 21:38:17 +0800

    Fix: unit test

--- packages/dateTimePicker/tests/unit/App.spec.js
+++ packages/dateTimePicker/tests/unit/App.spec.js
@@ -2,11 +2,11 @@ import { shallowMount } from "@vue/test-utils";
 import App from "@/App.vue";
 
 describe("App.vue", () => {
-  it("should imoprt date-time-picker component", () => {
+  it("should imoprt <timepicker component", () => {
     const format = "hh:mm A";
     const wrapper = shallowMount(App, {
       propsData: { format }
     });
-    expect(wrapper.html().includes('date-time-picker')).toBe(true)
+    expect(wrapper.html().includes('<timepicker')).toBe(true)
   });
 });
