9c6aaf8ee93a54043b8d30d44f7d2b9de68afa72
AnnaHuang <anna.huang@oriente.com>
2018-12-27 14:12:12 +0800

    add trigger

--- packages/dateTimePicker/src/components/Icons/Arrow.vue
+++ packages/dateTimePicker/src/components/Icons/Arrow.vue
@@ -1,13 +1,6 @@
 <template>
   <div>
-    <svg
-      width="7px"
-      height="13px"
-      viewBox="0 0 7 13"
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
-    >
+    <svg width="7px" height="13px" viewBox="0 0 7 13">
       <g
         id="icon-left"
         stroke="none"
