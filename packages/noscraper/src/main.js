202d9e275fbb08d3551180de296854d0af49e39f
waheng chang <wahengchang@gmail.com>
2019-01-02 11:42:02 +0800

    update noscraper plugin

--- packages/noscraper/src/main.js
+++ packages/noscraper/src/main.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
+import noscraper from "./plugins/noscraper"
 
 Vue.config.productionTip = false;
+
+Vue.use(noscraper, () => {console.log('[WARNING] scraper is browsing ...')})
 
 new Vue({
   render: h => h(App)
