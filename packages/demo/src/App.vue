bf14211e24961e139b1d3a770b1d1792a23a8dce
waheng chang <wahengchang@gmail.com>
2019-01-08 15:37:22 +0800

    added leftNav to demo

--- packages/demo/src/App.vue
+++ packages/demo/src/App.vue
@@ -1,90 +1,18 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Demo of Date Time picker"/>
-
-    <section class='componentContainer'>
-      <button @click='tapIndex = 0'> TimePicker </button>
-      <button @click='tapIndex = 1'> Calender </button>
-      <button @click='tapIndex = 2'> DatePicker </button>
-      <button @click='tapIndex = 3'> DateTimePicker </button>
-    </section>
-
-    <section class='componentContainer' v-if="tapIndex === 0">
-      <h1>TimePicker </h1>
-      <TimePicker />
-    </section>
-
-    <section class='componentContainer' v-if="tapIndex === 1">
-      <h1>Calender </h1>
-      <Calender
-        :month="currentMonth"
-        :year="currentYear"
-        :startDate="startDate"
-        :endDate="endDate"
-      />
-    </section>
-
-    <section class='componentContainer' v-if="tapIndex === 2">
-      <h1>DatePicker </h1>
-        <DatePicker
-          :startDate="startDate"
-          :endDate="endDate"
-        />
-    </section>
-
-    <section class='componentContainer' v-if="tapIndex === 3">
-      <h1>DateTimePicker </h1>
-      <DateTimePicker
-        :startDate = "startDate"
-        :endDate = "endDate"
-        :startTime = "startTime"
-        :endTime = "endTime"
-      />
-    </section>
-
-
+    <left-nav />
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-import { HelloWorld2 } from "@lazy-copilot/noscraper";
-import {
-  DateTimePicker,
-  DatePicker,
-  TimePicker,
-  Calender
-} from "@lazy-copilot/datetimepicker";
-
+import LeftNav from "./components/LeftNav.vue";
 export default {
   name: "app",
   components: {
-    HelloWorld,
-    DateTimePicker,
-    DatePicker,
-    TimePicker,
-    Calender
+    LeftNav
   },
-  data: function() {
-    return {
-      tapIndex: 0,
-      currentMonth: 11,
-      currentYear: 2018,
-      startDate: new Date("2018-12-18"),
-      endDate: new Date("2018-12-11"),
-      startTime: {
-        hh: "05",
-        mm: "23",
-        A: "AM"
-      },
-      endTime: {
-        hh: "11",
-        mm: "21",
-        A: "PM"
-      }
-    };
-  }
-};
+}
 </script>
 
 <style>
@@ -94,7 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 .componentContainer {
---
+++ packages/demo/src/components/LeftNav.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <div :class="isNavOpen? 'nav openNav': 'nav'">
+      <a href="javascript:void(0)" class="closebtn" @click="isNavOpen=false">&times;</a>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+    <span v-if="!isNavOpen" class="burger" @click="isNavOpen=!isNavOpen">&#9776; open</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "leftNave",
+  data: function(){
+    return { isNavOpen: false}
+  }
+};
+</script>
+    
+<style lang="scss" scoped>
+.nav {
+  width: 250px;
+  height: 100%;
+  position: fixed; 
+  z-index: 1; 
+  top: 0;
+  left: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  padding-top: 60px;
+  transition: 0.5s;
+
+  a {
+    padding: 8px 8px 8px 32px;
+    text-decoration: none;
+    color: #818181;
+    display: block;
+    transition: 0.3s;
+  }
+
+  a:hover {
+    color: #f1f1f1;
+  }
+
+  .closebtn {
+    display: none;
+  }
+}
+
+.burger {
+  cursor: pointer;
+  left: 0;
+  position: fixed;
+  top: 0;
+  margin: 10px 20px;
+}
+
+@media screen and (max-width: 600px) {
+  .nav {
+    padding-top: 30px;
+    width: 0;
+
+    a {
+      font-size: 18px;
+    }
+
+    .closebtn {
+      position: absolute;
+      top: 0;
+      right: 25px;
+      font-size: 36px;
+      margin-left: 50px;
+    }
+  }
+
+  .nav.openNav {
+    .closebtn{
+      display: block
+    }
+  }
+}
+
+.openNav {
+  width: 100%;
+}
+</style>
